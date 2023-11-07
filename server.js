b78708a2c161fdf7e77e52c4a51a24d671072583
tebri-artanto <tebriartanto@gmail.com>
2023-11-07 11:33:53 +0000

    commit from gcloud

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
     res.send("Response Success!!!")
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log("Server is up and listening on " + PORT)
 })
